94b2ce430d34abafbbd84a02fec57347c0583514
Conyafer <elout4@163.com>
2026-03-13 19:14:25 +0800

    rebuild the project

--- Server/src/main/java/com/test/http_spring/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.test.http_spring.service;
-
-import org.springframework.mail.SimpleMailMessage;
-
-public interface MailService {
-    void sendMail(SimpleMailMessage mailMessage);
-}
--- Server/src/main/resources/sql/fastfunctioninit.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO fast_functions(title, href, img) VALUES ('公告', 'https://www.baidu.com', '../../static/notice.png');
